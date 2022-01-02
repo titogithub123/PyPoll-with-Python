82b916d0dd78ff2d631b0c4224a8310fc756321e
titogithub123 <93171738+titogithub123@users.noreply.github.com>
2022-01-02 11:24:47 -0800

    Upload First Commit

---
+++ Practice Folder/Python_pracitce.py
@@ -0,0 +1,23 @@
+#print("Hello World")
+
+#temperature = int(input("What is the temperature outside?"))
+
+#if temperature > 80:
+    #print("Turn on the AC.")
+#else:
+    #print("Open the Windows.")
+score = int(input("What is your test score?"))
+
+if score >= 90:
+    print('Your grade is an A.')
+elif score>= 80:
+    print('Your grade is a B')
+elif score>= 70:
+    print('Your grade is a C')
+elif score>= 60:
+    print('Your grade is a D')
+else:
+    print('You Suck,, Drop Out of School')
+    
+
+
